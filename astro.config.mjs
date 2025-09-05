08fc14918694ea1f300282b69eb9bad09bec3a8e
Brice Ramdani <ramdani.brice@outlook.com>
2025-09-05 10:14:45 +0200

    Correctifs

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,9 @@ import partytown from "@astrojs/partytown";
 export default defineConfig({
   site: 'https://devolio.devaradise.com',
   integrations: [mdx(), sitemap(), tailwind(), partytown()],
+  build: {
+    outDir: 'build',
+  },
   markdown: {
     extendDefaultPlugins: true,
     rehypePlugins: [[autoNewTabExternalLinks, {
